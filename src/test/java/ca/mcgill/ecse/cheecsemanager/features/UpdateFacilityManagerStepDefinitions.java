fce598824bbf18ee8ced40f43effa5cefdbc0d14
mingli202 <vincentmingli@gmail.com>
2025-10-03 22:00:04 -0400

    format idk

--- src/test/java/ca/mcgill/ecse/cheecsemanager/features/UpdateFacilityManagerStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/cheecsemanager/features/UpdateFacilityManagerStepDefinitions.java
@@ -18,21 +18,27 @@ public class UpdateFacilityManagerStepDefinitions {
     throw new io.cucumber.java.PendingException();
   }
 
-  @When("the facility manager attempts to update the facility manager password to {string} \\(p2)")
-  public void the_facility_manager_attempts_to_update_the_facility_manager_password_to_p2(
+  @When("the facility manager attempts to update the facility manager "
+        + "password to {string} \\(p2)")
+  public void
+  the_facility_manager_attempts_to_update_the_facility_manager_password_to_p2(
       String string) {
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
   }
 
   @Then("the number of facility managers in the system shall be {int} \\(p2)")
-  public void the_number_of_facility_managers_in_the_system_shall_be_p2(Integer int1) {
+  public void
+  the_number_of_facility_managers_in_the_system_shall_be_p2(Integer int1) {
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
   }
 
-  @Then("the facility manager with password {string} shall exist in the system \\(p2)")
-  public void the_facility_manager_with_password_shall_exist_in_the_system_p2(String string) {
+  @Then("the facility manager with password {string} shall exist in the "
+        + "system \\(p2)")
+  public void
+  the_facility_manager_with_password_shall_exist_in_the_system_p2(
+      String string) {
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
   }
