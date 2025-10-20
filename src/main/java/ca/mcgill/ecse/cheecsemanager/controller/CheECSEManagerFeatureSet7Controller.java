519ec0cbd266d92541556eff4204c1c7ca1be9c4
Ewen <ewengue1432@gmail.com>
2025-10-19 23:37:39 -0400

    update and delete farmer implementation

--- src/main/java/ca/mcgill/ecse/cheecsemanager/controller/CheECSEManagerFeatureSet7Controller.java
+++ src/main/java/ca/mcgill/ecse/cheecsemanager/controller/CheECSEManagerFeatureSet7Controller.java
@@ -1,22 +1,53 @@
 package ca.mcgill.ecse.cheecsemanager.controller;
 
 import java.util.List;
-import java.util.stream.Stream;
 import ca.mcgill.ecse.cheecsemanager.application.CheECSEManagerApplication;
 import ca.mcgill.ecse.cheecsemanager.model.Farmer;
+import ca.mcgill.ecse.cheecsemanager.model.Purchase;
 
 public class CheECSEManagerFeatureSet7Controller {
 
-  public static String updateFarmer(String email, String newPassword, String newName,
-      String newAddress) {
-
-        
-
-    throw new UnsupportedOperationException("Implement me!");
+  public static String updateFarmer(String email, String newPassword, String newName,String newAddress) {
+    if (newPassword.isEmpty()) {
+      throw new IllegalArgumentException("Password must not be empty.");
+    }
+    if (newAddress.isEmpty()) {
+      throw new IllegalArgumentException("Address must not be empty.");
+    }
+    
+    var app = CheECSEManagerApplication.getCheecseManager();
+    List<Farmer> farmers = app.getFarmers();
+    for (Farmer farmer : farmers) {
+      if (email.equals(farmer.getEmail())) {
+        farmer.setPassword(newPassword);
+        farmer.setName(newName);
+        farmer.setAddress(newAddress);
+        return "";
+      }
+    }
+    throw new IllegalArgumentException("The farmer with email " + email + " does not exist.");
   }
 
   public static String deleteFarmer(String email) {
-    throw new UnsupportedOperationException("Implement me!");
+    var app = CheECSEManagerApplication.getCheecseManager();
+    List<Farmer> farmers = app.getFarmers();
+    boolean isRemoved = false;
+    for (Farmer farmer : farmers) {
+      if (email.equals(farmer.getEmail())) {
+        List<Purchase> purchases = farmer.getPurchases();
+
+        for (Purchase purchase : purchases) {
+          if (purchase.hasCheeseWheels()) {
+            throw new IllegalArgumentException("Cannot delete farmer who has supplied cheese.");
+          }
+        }
+        isRemoved = app.removeFarmer(farmer);
+      }
+    }
+    if (!isRemoved) {
+      throw new IllegalArgumentException("The farmer with email " + email +" does not exist.");
+    }
+    return "";
   }
 
   public static TOFarmer getFarmer(String email) {
