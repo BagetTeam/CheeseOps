d9a28f840b63178b931d3c5fc1dcdee683cf160e
mingli202 <vincentmingli@gmail.com>
2025-10-20 22:51:54 -0400

    fix controller set 3

--- src/main/java/ca/mcgill/ecse/cheecsemanager/controller/CheECSEManagerFeatureSet3Controller.java
+++ src/main/java/ca/mcgill/ecse/cheecsemanager/controller/CheECSEManagerFeatureSet3Controller.java
@@ -3,55 +3,65 @@ package ca.mcgill.ecse.cheecsemanager.controller;
 import ca.mcgill.ecse.cheecsemanager.application.CheECSEManagerApplication;
 import ca.mcgill.ecse.cheecsemanager.model.CheeseWheel;
 import ca.mcgill.ecse.cheecsemanager.model.Farmer;
-
 import java.util.List;
 /**
  * @author David Tang
  * */
 public class CheECSEManagerFeatureSet3Controller {
-
-
-  public static String registerFarmer(String email, String password, String name, String address) {
+  public static String registerFarmer(String email, String password,
+                                      String name, String address) {
     // checks for existing farmer
     var app = CheECSEManagerApplication.getCheecseManager();
-    for (var farmer : app.getFarmers()) {
-      if (email.equals(farmer.getEmail())) {
-        return "The farmer email already exists.";
-      }
+    if (Farmer.hasWithEmail(email)) {
+      return "The farmer email already exists.";
     }
 
-    //ensures that all the error messages shown in the feature file for register farmer are thrown
-    if (!email.contains("@")) return "Email must contain @ symbol.";
+    // ensures that all the error messages shown in the feature file for
+    // register farmer are thrown
+    if (!email.contains("@"))
+      return "Email must contain @ symbol.";
     int atLocation = email.indexOf("@");
-    if (atLocation-1 <=0 ) return "Email must have characters before @.";
-    else if (!email.substring(atLocation).contains(".")) return "Email must contain a dot after @.";
-    else if (email.substring(atLocation).indexOf(".")+1 >= email.substring(atLocation).length()) return "Email must have characters after dot.";
-    else if (email.contains(" ")) return "Email must not contain spaces.";
-    else if (email.equals("manager@cheecse.fr")) return "Email cannot be manager@cheecse.fr.";
-    else if (password==null || password.isBlank()) return "Password must not be empty.";
-    else if (address==null || address.isBlank()) return "Address must not be empty.";
+    if (atLocation - 1 <= 0)
+      return "Email must have characters before @.";
+    else if (!email.substring(atLocation).contains("."))
+      return "Email must contain a dot after @.";
+    else if (email.substring(atLocation).indexOf(".") + 1 >=
+             email.substring(atLocation).length())
+      return "Email must have characters after dot.";
+    else if (email.contains(" "))
+      return "Email must not contain spaces.";
+    else if (email.equals("manager@cheecse.fr"))
+      return "Email cannot be manager@cheecse.fr.";
+    else if (password == null || password.isBlank())
+      return "Password must not be empty.";
+    else if (address == null || address.isBlank())
+      return "Address must not be empty.";
 
-    // if no issues with registration, create a new farmer and add it to the farmer List in the application
-    Farmer farmer = new Farmer(email, password, address, app.getManager().getCheECSEManager());
+    // if no issues with registration, create a new farmer and add it to the
+    // farmer List in the application
+    Farmer farmer = new Farmer(email, password, address,
+                               app.getManager().getCheECSEManager());
     farmer.setName(name);
     app.addFarmer(farmer);
 
-    //return no error message
+    // return no error message
     return "";
   }
 
-  public static String updateCheeseWheel(Integer cheeseWheelID, String newMonthsAged,
-      Boolean newIsSpoiled) {
+  public static String updateCheeseWheel(Integer cheeseWheelID,
+                                         String newMonthsAged,
+                                         Boolean newIsSpoiled) {
     var app = CheECSEManagerApplication.getCheecseManager();
     for (var cheeseWheel : app.getCheeseWheels()) {
-      //checks list for matching cheese ID to ensure it even exists
+      // checks list for matching cheese ID to ensure it even exists
       if (cheeseWheelID.equals(cheeseWheel.getId())) {
         // Parse and validate monthsAged
         CheeseWheel.MaturationPeriod updatedPeriod;
         try {
           updatedPeriod = CheeseWheel.MaturationPeriod.valueOf(newMonthsAged);
         } catch (IllegalArgumentException e) {
-          return "The monthsAged must be Six, Twelve, TwentyFour, or ThirtySix.";
+          return "The monthsAged must be Six, Twelve, TwentyFour, or "
+              + "ThirtySix.";
         }
 
         if (updatedPeriod.ordinal() < cheeseWheel.getMonthsAged().ordinal()) {
@@ -63,11 +73,11 @@ public class CheECSEManagerFeatureSet3Controller {
 
         if (newIsSpoiled) {
           if (cheeseWheel.getLocation() != null) {
-            //deletes the cheese wheel from the shelf location.
+            // deletes the cheese wheel from the shelf location.
             cheeseWheel.getLocation().setCheeseWheel(null);
           }
           if (cheeseWheel.getOrder() != null) {
-            //deletes the cheese wheel from the order.
+            // deletes the cheese wheel from the order.
             cheeseWheel.getOrder().removeCheeseWheel(cheeseWheel);
           }
         }
@@ -81,18 +91,31 @@ public class CheECSEManagerFeatureSet3Controller {
   public static TOCheeseWheel getCheeseWheel(Integer cheeseWheelID) {
     var app = CheECSEManagerApplication.getCheecseManager();
     for (var cheeseWheel : app.getCheeseWheels()) {
-      // if the ID can be found in the List of cheeseWheels, then create a TOCheeseWheel instance with its constructor.
+      // if the ID can be found in the List of cheeseWheels, then create a
+      // TOCheeseWheel instance with its constructor.
       if (cheeseWheelID.equals(cheeseWheel.getId())) {
         /* use this if location can be null to avoid exceptions
           var location = cheeseWheel.getLocation();
-          String shelfId = location != null ? location.getShelf().getId() : null;
+          String shelfId = location != null ? location.getShelf().getId() :
+        null;
 
-        this may cause null pointer since collumn and row are int, which cannot be set to null in Java...
-          Integer column = location != null ? location.getColumn() : null;
-          Integer row = location != null ? location.getRow() : null;
-        return new TOCheeseWheel(cheeseWheelID, cheeseWheel.getMonthsAged().name(), cheeseWheel.isIsSpoiled(), cheeseWheel.getPurchase().getTransactionDate(), shelfId, column, row, cheeseWheel.hasOrder());
+        this may cause null pointer since collumn and row are int, which cannot
+        be set to null in Java... Integer column = location != null ?
+        location.getColumn() : null; Integer row = location != null ?
+        location.getRow() : null; return new TOCheeseWheel(cheeseWheelID,
+        cheeseWheel.getMonthsAged().name(), cheeseWheel.isIsSpoiled(),
+        cheeseWheel.getPurchase().getTransactionDate(), shelfId, column, row,
+        cheeseWheel.hasOrder());
         */
-        return new TOCheeseWheel(cheeseWheel.getId(), cheeseWheel.getMonthsAged().name(), cheeseWheel.isIsSpoiled(), cheeseWheel.getPurchase().getTransactionDate(), cheeseWheel.getLocation().getShelf().getId(), cheeseWheel.getLocation().getColumn(), cheeseWheel.getLocation().getRow(), cheeseWheel.hasOrder());
+        var location = cheeseWheel.getLocation();
+
+        return new TOCheeseWheel(
+            cheeseWheel.getId(), cheeseWheel.getMonthsAged().name(),
+            cheeseWheel.isIsSpoiled(),
+            cheeseWheel.getPurchase().getTransactionDate(),
+            location == null ? null : location.getShelf().getId(),
+            location == null ? -1 : location.getColumn(),
+            location == null ? -1 : location.getRow(), cheeseWheel.hasOrder());
       }
     }
     return null;
@@ -102,10 +125,21 @@ public class CheECSEManagerFeatureSet3Controller {
   public static List<TOCheeseWheel> getCheeseWheels() {
     var app = CheECSEManagerApplication.getCheecseManager();
 
-    // returns a List of TOCheeseWheels created from the CheeseWheel List stored in the application.
+    // returns a List of TOCheeseWheels created from the CheeseWheel List stored
+    // in the application.
     return app.getCheeseWheels()
-            .stream()
-            .map(cheeseWheel -> new TOCheeseWheel(cheeseWheel.getId(), cheeseWheel.getMonthsAged().name(), cheeseWheel.isIsSpoiled(), cheeseWheel.getPurchase().getTransactionDate(), cheeseWheel.getLocation().getShelf().getId(), cheeseWheel.getLocation().getColumn(), cheeseWheel.getLocation().getRow(), cheeseWheel.hasOrder()))
-            .toList();
+        .stream()
+        .map(cheeseWheel -> {
+          var location = cheeseWheel.getLocation();
+          return new TOCheeseWheel(
+              cheeseWheel.getId(), cheeseWheel.getMonthsAged().name(),
+              cheeseWheel.isIsSpoiled(),
+              cheeseWheel.getPurchase().getTransactionDate(),
+              location == null ? null : location.getShelf().getId(),
+              location == null ? -1 : location.getColumn(),
+              location == null ? -1 : location.getRow(),
+              cheeseWheel.hasOrder());
+        })
+        .toList();
   }
 }
