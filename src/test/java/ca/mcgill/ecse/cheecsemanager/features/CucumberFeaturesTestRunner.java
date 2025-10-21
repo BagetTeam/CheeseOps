f6c77dacafaadba17f679a768a7170651506c1b5
Eun-Jun Chang <eun-jun.chang@mail.mcgill.ca>
2025-10-20 21:41:52 -0400

    Fixed controller4 code

--- src/test/java/ca/mcgill/ecse/cheecsemanager/features/CucumberFeaturesTestRunner.java
+++ src/test/java/ca/mcgill/ecse/cheecsemanager/features/CucumberFeaturesTestRunner.java
@@ -5,6 +5,12 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+<<<<<<< Updated upstream
 @CucumberOptions(plugin = "pretty", features = "src/test/resources/02.AddShelf.feature",
+=======
+@CucumberOptions(plugin = "pretty",
+                 features =
+                     "src/test/resources/10.AssignAndRemoveCheeseWheel.feature",
+>>>>>>> Stashed changes
                  glue = "ca.mcgill.ecse.cheecsemanager.features")
 public class CucumberFeaturesTestRunner {}
