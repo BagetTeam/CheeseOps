812c465717949dae50cec970ced6505a12d54f20
stupidavid <tangdavid03@gmail.com>
2025-11-01 15:59:01 -0400

    Created the persistence.ump file, and all the files needed in the ca.mcgill.ecse.cheecsemanager.persistence package. Ensured that the cheecsemanager.ump file uses the persistence umple file, completed the reinitialize code, have yet to generate the new Java code in the model package.

---
+++ src/main/java/ca/mcgill/ecse/cheecsemanager/persistence/CheECSEManagerPersistence.java
@@ -0,0 +1,34 @@
+package ca.mcgill.ecse.cheecsemanager.persistence;
+
+import ca.mcgill.ecse.cheecsemanager.application.CheECSEManagerApplication;
+import ca.mcgill.ecse.cheecsemanager.model.CheECSEManager;
+
+public class CheECSEManagerPersistence {
+
+  private static String filename = "data.json";
+  private static JsonSerializer serializer = new JsonSerializer("ca.mcgill.ecse.cheecsemanager");
+
+  public static void setFilename(String filename) {
+    CheECSEManagerPersistence.filename = filename;
+  }
+
+  public static void save() {
+    save(CheECSEManagerApplication.getCheecseManager());
+  }
+
+  public static void save(CheECSEManager cheECSEManager) {
+    serializer.serialize(cheECSEManager, filename);
+  }
+
+  public static CheECSEManager load() {
+    var cheECSEManager = (CheECSEManager) serializer.deserialize(filename);
+    // model cannot be loaded - create empty
+    if (cheECSEManager == null) {
+      cheECSEManager = new CheECSEManager();
+    } else {
+      cheECSEManager.reinitialize();
+    }
+    return cheECSEManager;
+  }
+
+}
