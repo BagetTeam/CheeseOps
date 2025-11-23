efee7bb7abf0c81911eec644505676e81b6bb7a2
mingli202 <vincentmingli@gmail.com>
2025-11-22 22:20:04 -0500

    buttons igs

--- src/main/java/ca/mcgill/ecse/cheecsemanager/fxml/components/StyledButton.java
+++ src/main/java/ca/mcgill/ecse/cheecsemanager/fxml/components/StyledButton.java
@@ -1,14 +1,16 @@
 package ca.mcgill.ecse.cheecsemanager.fxml.components;
 
 import ca.mcgill.ecse.cheecsemanager.application.CheECSEManagerApplication;
+import java.io.IOException;
+import javafx.beans.NamedArg;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
-import javafx.scene.Node;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 
 public class StyledButton extends Button {
   public enum Variant { DEFAULT, PRIMARY, DESTRUCTIVE, MUTED }
-  public enum Size { BASE, SM, LG, THIN }
+  public enum Size { XS, SM, BASE, MD, LG }
 
   private final StringProperty variant =
       new SimpleStringProperty(Variant.DEFAULT.name());
@@ -21,31 +23,43 @@ public class StyledButton extends Button {
     this.initialize();
   }
 
-  public StyledButton(String text) {
+  public StyledButton(@NamedArg("variant") Variant variant,
+                      @NamedArg("size") Size size,
+                      @NamedArg("text") String text) {
     super(text);
+    this.loadFXML();
     this.initialize();
-  }
 
-  public StyledButton(String text, Node graphic) {
-    super(text, graphic);
-    this.initialize();
+    this.variant.set(variant.name());
+    this.size.set(size.name());
   }
 
   public void initialize() {
-    getStylesheets().add(
-        getClass()
-            .getResource(CheECSEManagerApplication.PACKAGE_ID.concat(
-                "view/components/StyledButton/StyledButton.css"))
-            .toExternalForm());
+    getStyleClass().add("button");
 
     this.variant.addListener((observable, oldValue, newValue) -> {
-      this.setStyle("-fx-background-color: " + this.variant.get() +
-                    "; -fx-text-fill: " + this.variant.get());
+      getStyleClass().removeAll("button-default", "button-primary",
+                                "button-destructive", "button-muted");
+      getStyleClass().add("button-".concat(this.variant.get().toLowerCase()));
     });
 
     this.size.addListener((observable, oldValue, newValue) -> {
-      this.setStyle("-fx-font-size: " + this.size.get() +
-                    "; -fx-padding: " + this.size.get() + ";");
+      getStyleClass().removeAll("button-base", "button-sm", "button-lg",
+                                "button-md", "button-xs");
+      String sizeClass = "button-".concat(this.size.get().toLowerCase());
+      getStyleClass().add(sizeClass);
     });
+  }
+
+  private void loadFXML() {
+    FXMLLoader loader = new FXMLLoader(CheECSEManagerApplication.getResource(
+        "view/components/StyledButton/StyledButton.fxml"));
+    loader.setRoot(this);
+    loader.setController(this);
+    try {
+      loader.load();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
