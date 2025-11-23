c2be6c60270d5c3a9d7ac5291dce9c1747b9132f
stupidavid <tangdavid03@gmail.com>
2025-11-22 22:29:15 -0500

    created robot fxml class

--- src/main/java/ca/mcgill/ecse/cheecsemanager/fxml/controllers/MainController.java
+++ src/main/java/ca/mcgill/ecse/cheecsemanager/fxml/controllers/MainController.java
@@ -1,4 +1,15 @@
 package ca.mcgill.ecse.cheecsemanager.fxml.controllers;
 
+import javafx.scene.control.*;
+
 public class MainController {
+
+
+    private Button button1;
+
+
+    private void ee(){
+
+
+    }
 }
