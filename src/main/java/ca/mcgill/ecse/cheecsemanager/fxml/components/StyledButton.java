a964f15ccc338d6208c8992cdd5c044b553bfb3c
mingli202 <vincentmingli@gmail.com>
2025-11-22 20:32:12 -0500

    tmp

---
+++ src/main/java/ca/mcgill/ecse/cheecsemanager/fxml/components/StyledButton.java
@@ -0,0 +1,49 @@
+package ca.mcgill.ecse.cheecsemanager.fxml.components;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+
+public class StyledButton extends Button {
+  public enum Variant { DEFAULT, PRIMARY, DESTRUCTIVE, MUTED }
+  public enum Size { BASE, SM, LG, THIN }
+
+  private final StringProperty variant =
+      new SimpleStringProperty(Variant.DEFAULT.name());
+
+  private final StringProperty size =
+      new SimpleStringProperty(Size.BASE.name());
+
+  public StyledButton() {
+    super();
+    this.initialize();
+  }
+
+  public StyledButton(String text) {
+    super(text);
+    this.initialize();
+  }
+
+  public StyledButton(String text, Node graphic) {
+    super(text, graphic);
+    this.initialize();
+  }
+
+  public void initialize() {
+    getStylesheets().add(
+        getClass()
+            .getResource("view/components/StyledButton/StyledButton.css")
+            .toExternalForm());
+
+    this.variant.addListener((observable, oldValue, newValue) -> {
+      this.setStyle("-fx-background-color: " + this.variant.get() +
+                    "; -fx-text-fill: " + this.variant.get());
+    });
+
+    this.size.addListener((observable, oldValue, newValue) -> {
+      this.setStyle("-fx-font-size: " + this.size.get() +
+                    "; -fx-padding: " + this.size.get() + ";");
+    });
+  }
+}
