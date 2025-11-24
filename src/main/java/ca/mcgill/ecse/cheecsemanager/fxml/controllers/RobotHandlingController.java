4fdd0a1a47f98497668ee8a474a123db7d3211bc
Benjamin <benjamincf01@gmail.com>
2025-11-24 02:44:49 -0500

    ui for robot page

--- src/main/java/ca/mcgill/ecse/cheecsemanager/fxml/controllers/RobotHandlingController.java
+++ src/main/java/ca/mcgill/ecse/cheecsemanager/fxml/controllers/RobotHandlingController.java
@@ -1,65 +1,39 @@
 package ca.mcgill.ecse.cheecsemanager.fxml.controllers;
 
-
-import javafx.animation.*;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Paint;
-import javafx.util.Duration;
+import javafx.scene.layout.StackPane;
 
 public class RobotHandlingController {
 
-    public AnchorPane activateScreen;
-    public AnchorPane initializeScreen;
-    public AnchorPane deactivateScreen;
+    @FXML
+    private StackPane activateTile;
+    @FXML
+    private StackPane deactivateTile;
+    @FXML
+    private StackPane initializeTile;
 
-    public Label statusDisplay;
+    private final BooleanProperty robotActive = new SimpleBooleanProperty(false);
 
-    public Button activateRobot;
-    public Button initializeRobot;
-    public Button deactivateRobot;
-
-    public Button backToMenu;
-
-
-    public void activateRobot(){
-        this.transition(activateScreen, "Status: Activated", "#92e090");
+    @FXML
+    private void initialize() {
+        bindPowerTiles();
+        wireTileInteractions();
     }
 
-    public void initializeRobot(){
-        this.transition(initializeScreen, "Status: Initialized", "#ffda6d");
+    private void bindPowerTiles() {
+        activateTile.visibleProperty().bind(robotActive.not());
+        activateTile.managedProperty().bind(robotActive.not());
+
+        deactivateTile.visibleProperty().bind(robotActive);
+        deactivateTile.managedProperty().bind(robotActive);
+
+        initializeTile.disableProperty().bind(robotActive.not());
     }
 
-    public void deactivateRobot(){
-        this.transition(deactivateScreen, "Status: Deactivated","#f07e68");
+    private void wireTileInteractions() {
+        activateTile.setOnMouseClicked(event -> robotActive.set(true));
+        deactivateTile.setOnMouseClicked(event -> robotActive.set(false));
     }
-
-
-
-    public void transition(Pane coverPane, String newStatus, String paint){
-        coverPane.setVisible(true);
-        FadeTransition fade = new FadeTransition(new Duration(1500), coverPane);
-        FadeTransition fade2 = new FadeTransition(new Duration(1500), coverPane);
-        fade.setFromValue(0);
-        fade.setToValue(1);
-        fade.play();
-
-
-        fade.setOnFinished(e -> {
-            fade2.setFromValue(1);
-            fade2.setToValue(0);
-            fade2.play();
-            statusDisplay.setText(newStatus);
-            statusDisplay.setTextFill(Paint.valueOf(paint));
-        });
-
-        fade2.setOnFinished(e ->{
-            coverPane.setVisible(false);
-        });
-    }
-
-
-
-    public void returnToMenu(){}
 }
