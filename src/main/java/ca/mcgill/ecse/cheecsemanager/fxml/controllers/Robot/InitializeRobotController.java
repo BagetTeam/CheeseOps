e5d833416c833fe59c6218b83b90fd0211c0ba76
Benjamin <benjamincf01@gmail.com>
2025-11-28 21:01:12 -0500

    working robot initialization

--- src/main/java/ca/mcgill/ecse/cheecsemanager/fxml/controllers/Robot/InitializeRobotController.java
+++ src/main/java/ca/mcgill/ecse/cheecsemanager/fxml/controllers/Robot/InitializeRobotController.java
@@ -1,6 +1,10 @@
 package ca.mcgill.ecse.cheecsemanager.fxml.controllers.Robot;
 import ca.mcgill.ecse.cheecsemanager.fxml.components.Dropdown;
 import ca.mcgill.ecse.cheecsemanager.fxml.components.StyledButton;
+import ca.mcgill.ecse.cheecsemanager.fxml.events.HidePopupEvent;
+import ca.mcgill.ecse.cheecsemanager.fxml.events.ToastEvent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import ca.mcgill.ecse.cheecsemanager.model.Shelf;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,8 +14,11 @@ import ca.mcgill.ecse.cheecsemanager.controller.CheECSEManagerFeatureSet1Control
 import javafx.fxml.FXML;
 
 public class InitializeRobotController {
+
+  @FXML private VBox root;
   @FXML private Dropdown ShelfIdDropdown;
   @FXML private StyledButton startBtn;
+  @FXML private Label errorLabel;
 
   @FXML
   public void initialize() {
@@ -19,6 +26,10 @@ public class InitializeRobotController {
 
     List<String> items = CheECSEManagerFeatureSet1Controller.getAllShelfIds();
     ShelfIdDropdown.setItems(items);
+  }
+
+  private void closePopup() {
+    root.fireEvent(new HidePopupEvent());
   }
 
   private void bindings() {
@@ -31,6 +42,29 @@ public class InitializeRobotController {
 
   private void submit() {
     String shelfIdVal = ShelfIdDropdown.getSelectedValue();
-    
+
+    // this is pretty useless cuz there
+    if (shelfIdVal == null || shelfIdVal.isBlank()) {
+      showError("Please fill in all fields.");
+    }
+
+    try {
+      RobotController.initializeRobot(shelfIdVal);
+      root.fireEvent(new ToastEvent("Robot initialized succsessfully.",
+                            ToastEvent.ToastType.SUCCESS));
+      closePopup();
+    } catch (RuntimeException e) {
+      errorLabel.setText(e.getMessage());
+    }
+  }
+
+    private void showError(String message) {
+    if (errorLabel != null) {
+      errorLabel.setText(message);
+      errorLabel.setVisible(true);
+      errorLabel.setManaged(true);
+    } else {
+      System.err.println("Treatment Popup Error: " + message);
+    }
   }
 }
