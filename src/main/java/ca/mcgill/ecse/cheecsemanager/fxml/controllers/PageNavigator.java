34a812a152d3b0e029766251973786ca1d2b1fde
Ewen <ewengue1432@gmail.com>
2025-12-01 15:49:48 -0500

    sm animation

--- src/main/java/ca/mcgill/ecse/cheecsemanager/fxml/controllers/PageNavigator.java
+++ src/main/java/ca/mcgill/ecse/cheecsemanager/fxml/controllers/PageNavigator.java
@@ -1,10 +1,14 @@
 package ca.mcgill.ecse.cheecsemanager.fxml.controllers;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import java.io.IOException;
 import java.util.Stack;
+import java.util.concurrent.Callable;
+import ca.mcgill.ecse.cheecsemanager.fxml.components.Animation.AnimationManager;
+import ca.mcgill.ecse.cheecsemanager.fxml.components.Animation.EasingInterpolators;
 
 /**
  * Controller that handles navigating between pages
@@ -37,6 +41,14 @@ public class PageNavigator {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
             Pane newPage = loader.load();
             
+            AnimationManager.numericBuilder()
+                .target(newPage.translateXProperty())
+                .from(contentArea.getWidth())
+                .to(0)
+                .durationMillis(500)
+                .easing(EasingInterpolators.CUBIC_OUT)
+                .play();
+
             // If controller has a setData method, call it
             if (data != null) {
                 Object controller = loader.getController();
@@ -60,12 +72,26 @@ public class PageNavigator {
         }
     }
     
-    public void goBack() {
+    public void goBack(boolean animate) {
         if (!navigationStack.isEmpty()) {
             Pane previousPage = navigationStack.pop();
-            contentArea.getChildren().clear();
-            contentArea.getChildren().add(previousPage);
-            
+            Node curPage = contentArea.getChildren().get(0);
+            if (animate) {
+                AnimationManager.numericBuilder()
+                    .target(curPage.translateXProperty())
+                    .from(0)
+                    .to(contentArea.getWidth())
+                    .durationMillis(500)
+                    .easing(EasingInterpolators.CUBIC_OUT)
+                    .onFinished(() -> {
+                        contentArea.getChildren().clear();
+                        contentArea.getChildren().add(previousPage);
+                    }).play();
+                }
+                else {
+                    contentArea.getChildren().clear();
+                    contentArea.getChildren().add(previousPage);
+                }
             // If the page supports refresh, call it
             Object userData = previousPage.getUserData();
             if (userData instanceof PageRefreshable) {
